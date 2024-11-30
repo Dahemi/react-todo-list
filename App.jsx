ff4005b6ebe9e94067662d251dadf7264e0337d2
Dahami <126935880+Dahemi@users.noreply.github.com>
2024-11-30 16:34:44 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import Button from "./Button.jsx";
+import Counter from "./Counter";
+import MyComponent from "./MyComponent.jsx";
+import ColorPicker from "./ColorPicker.jsx";
+import Demo from "./Demo.jsx";
+import ToDoList from "./ToDoList/ToDoList.jsx";
+function App() {
+  return (
+    <>
+      <ToDoList />
+    </>
+  );
+}
+export default App;
